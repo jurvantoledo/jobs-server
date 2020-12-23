55b198f860824c921122e719a93871f60476be7e
jurvantoledo <jurvantoledo@live.nl>
2020-12-23 17:45:12 +0100

    made new route for weapons

--- routers/weapons.js
+++ routers/weapons.js
@@ -17,4 +17,24 @@ router.get("/", async(req, res, next) => {
     }
 })
 
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    console.log(id);
+    if (isNaN(parseInt(id))) {
+      return res.status(400).send({ message: "product id is not a number" });
+    }
+  
+    const weapon = await Weapon.findByPk(id, {
+      include: [Element],
+      order: [[Element, "createdAt", "DESC"]]
+    });
+  
+    if (weapon === null) {
+      return res.status(404).send({ message: "Product not found" });
+    }
+  
+    res.status(200).send({ message: "ok", weapon });
+  });
+
 module.exports = router;
