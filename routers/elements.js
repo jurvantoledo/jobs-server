522848e0265d39bfa31452c69e063d6f7558a16e
jurvantoledo <jurvantoledo@live.nl>
2020-12-24 15:44:30 +0100

    made delete route for elements

---
+++ routers/elements.js
@@ -0,0 +1,36 @@
+const { Router } = require("express")
+const Weapon = require("../models").weapon
+const Element = require("../models").element
+
+const router = new Router()
+
+router.get("/", async(req, res, next) => {
+    try {
+        const elements = await Element.findAll({
+            include: [Weapon],
+            order: [[Weapon, "createdAt", "DESC"]]
+        })
+        res.status(200).send({message: "ok", elements})
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+      const weaponId = req.params.id
+      const toDelete = await Element.findByPk(weaponId)
+  
+      if(!toDelete) {
+        res.status(404).send("reservation not found")
+      }
+  
+      const deleted = await toDelete.destroy()
+      res.json(deleted)
+    } catch (e) {
+      next(e)
+    }
+  });
+
+  module.exports = router;
