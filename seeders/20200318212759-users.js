cded97ca2d6393fbbfb2e578a67ec44e5359a455
jurvantoledo <jurvantoledo@live.nl>
2020-12-27 16:55:21 +0100

    changed user dropdown as test

--- seeders/20200318212759-users.js
+++ seeders/20200318212759-users.js
@@ -20,6 +20,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("users", null, {});
+    await queryInterface.bulkDelete("users", {});
   },
 };
