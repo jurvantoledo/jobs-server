a238eb9e0dda13110a49e2280d8050aa3b5e9fd6
jurvantoledo <jurvantoledo@live.nl>
2021-04-08 20:33:56 +0200

    added ssl

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ module.exports = {
   development: {
     url: "postgres://heuqqxgm:aiMxnr7M9tjHrwjnpAKqPsp3NyMohhU-@suleiman.db.elephantsql.com:5432/heuqqxgm",
     dialect: "postgres",
+    "operatorsAliases": "0"
   },
   test: {
     username: "root",
@@ -10,27 +11,9 @@ module.exports = {
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
+    "operatorsAliases": "0"
   },
   production: {
     use_env_variable: "DATABASE_URL",
   },
 };
-
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.ELEPHANT_SQL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
